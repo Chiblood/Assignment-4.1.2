615dc4d9e33ff956b585474a51307044995181dd
Chiblood <chi.blood6@gmail.com>
2025-09-17 11:21:57 -0400

    Retry the calculator

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
+/* Assignment 4.1.2
+ Create an interface for ICalculator for methods like add, subtract, divide, multiply and implement them in a class: Calculator.
+
+Use winforms to design a simple calculator with textboxes and buttons . Use the above designed interface in the calculator.
+ */
+
 namespace Assignment_4._1._2
 {
     internal static class Program
@@ -12,6 +18,29 @@ namespace Assignment_4._1._2
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+        }
+    }
+    public class Calculator : ICalculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Denominator cannot be zero.");
+            }
+            return a / b;
         }
     }
 }
